d2d5a886858ba01651f2b2c5fc8aa7e961b12e6b
Ahmed osman <ahmedothmanao571097@gmail.com>
2023-09-10 06:30:14 +0300

    finished radio screen

--- lib/radio/radio_tab.dart
+++ lib/radio/radio_tab.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
 class RadioTab extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+              child: Center(
+                  child: Image.asset('assets/images/radio-vector.png'),
+              ),
+          ),
+          Text(
+            'إذاعة القرآن الكريم',
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          Expanded(
+            child: Center(
+              child: Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.skip_next_rounded,
+                  size: 45,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  Icon(Icons.play_arrow,
+                    size: 55,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  Icon(Icons.skip_next_rounded,
+                    size: 45,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/tasbeh/tasbeh_tab.dart
+++ lib/tasbeh/tasbeh_tab.dart
@@ -1,12 +1,82 @@
 import 'package:flutter/material.dart';
 
-class TasbehTab extends StatelessWidget {
+class TasbehTab extends StatefulWidget {
+  @override
+  State<TasbehTab> createState() => _TasbehTabState();
+}
 
-
+class _TasbehTabState extends State<TasbehTab> {
+  int counter = 0;
+  int index = 0;
+  List<String> tasbeh = [
+    "سبحان الله",
+    "الحمد لله",
+    "الله اكبر",
+    "لا اله الا الله",
+    "لا اله الا أنت سيحانك أني كنت من الظالمين",
+    "لا حول ولا قوة الا بالله العلي العظيم",
+    "اللهم صلي علي سيدنا محمد",
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purple,
+    if (counter == 33 ||
+        counter == 66 ||
+        counter == 99 ||
+        counter == 133 ||
+        counter == 166 ||
+        counter == 200 ||
+        counter == 233) {
+      index++;
+
+    }
+
+    return Center(
+      child: Column(
+        children: [
+          Image.asset('assets/images/head_of_seb7a.png'),
+          InkWell(
+              onTap: () {
+                counter++;
+                setState(() {});
+              },
+              borderRadius: BorderRadius.circular(25),
+              child: Image.asset('assets/images/body_of_seb7a.png')),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'عدد التسبيحات',
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color(0xff948080),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Text(
+              '$counter',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(35),
+            ),
+            child: Text(
+              '${tasbeh[index]}',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
